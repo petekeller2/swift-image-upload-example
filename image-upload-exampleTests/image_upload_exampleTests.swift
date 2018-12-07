40c72d47381719c737fc13a0eb78fd306d6bfd99
Peter Keller <petekeller2@gmail.com>
2018-12-07 16:24:40 -0500

    Initial Commit

---
+++ image-upload-exampleTests/image_upload_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  image_upload_exampleTests.swift
+//  image-upload-exampleTests
+//
+//  Created by Peter Keller on 12/7/18.
+//  Copyright © 2018 Peter Keller. All rights reserved.
+//
+
+import XCTest
+@testable import image_upload_example
+
+class image_upload_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
