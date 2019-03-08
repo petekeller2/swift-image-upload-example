0626eef2c2b5cb28c05456546a9a017b57c089cc
Peter Keller <petekeller2@gmail.com>
2019-03-07 20:01:44 -0500

    Remove unused model

--- image-upload-example/Model/RequestsForLocImage.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RequestsForLocImage.swift
-//  image-upload-example
-//
-//  Created by Peter Keller on 12/14/18.
-//  Copyright © 2018 Peter Keller. All rights reserved.
-//
-
-import Foundation
